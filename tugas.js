49422179f2a526d0b55c95df6f768a961b1b178c
Fazakri <86667861+Fazakri@users.noreply.github.com>
2021-07-24 09:53:31 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,9 @@
+var nilai = 90
+var grade = "";
+
+if(nilai >= 90) grade = "A"
+else if(nilai >= 80) grade = "B+"
+else if(nilai >= 70) grade = "B"
+else grade = "F";
+
+console.log('Terbesar : ${grade}');
